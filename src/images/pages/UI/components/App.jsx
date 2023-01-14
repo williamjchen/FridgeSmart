85b51dc57728c44aa8bfe7b8d6069ab22501e93f
stanleyylin <stanleylin82@gmail.com>
2023-01-14 17:25:13 -0500

    index and setting up structure still

---
+++ src/images/pages/UI/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>
+      App!!
+    </div>
+  )
+}
+
+export default app
---
+++ src/images/pages/UI/components/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(<App/>)
