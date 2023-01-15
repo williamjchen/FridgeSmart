8b507042bc590ffc386331fc82150df5f991bbff
stanleyylin <stanleylin82@gmail.com>
2023-01-14 22:46:31 -0500

    hi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import './navbar.css'
 import Logo from '../images/logo.png'
 import {Link, NavLink} from 'react-router-dom'
+import Icon1 from '../images/icon1.png'
+import Icon2 from '../images/icon2.png'
+import Icon3 from '../images/icon3.png'
+import {GoThreeBars} from 'react-icons/go'
 
 const Navbar = () => {
   return (
@@ -10,13 +14,20 @@ const Navbar = () => {
           <p> FridgeSmart</p>
         </Link>
         <ul className='nav_links'>
-          <li>
-            <NavLink to='./pages/recipes'>Recipe</NavLink>
+        <li>
+            <NavLink to='./pages/recipes'><a href="" className="btn1"><img className="logo1"src={Icon1}/>Home</a></NavLink>
           </li>
           <li>
-            <NavLink to='./pages/recipes'>Recipe</NavLink>
+            <NavLink to='./pages/recipes'><a href="" className="btn2"><img className="logo2"src={Icon2}/>Recipe</a></NavLink>
+          </li>
+          <li>
+            <NavLink to='./pages/recipes'><a href="" className="btn1"><img className="logo1"src={Icon3}/>Recipe</a></NavLink>
           </li>
         </ul>
+
+        <button className="nav_toggle-btn">
+          <GoThreeBars/>
+        </button>
 
       </div>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
