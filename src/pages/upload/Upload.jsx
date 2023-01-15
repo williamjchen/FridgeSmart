a79f10d94d6c8123d5c31bc8f4d706d29e1e5230
William Chen <williamchenjian@gmail.com>
2023-01-15 01:22:19 -0500

    upload create recipe

--- src/pages/upload/Upload.jsx
+++ src/pages/upload/Upload.jsx
@@ -1,33 +1,40 @@
 import './upload.css'
 import axios from 'axios'
+import { useState } from 'react';
+import Recipes from '../recipes/Recipes';
+
 //import fs from 'fs'
 
-const processImage = () => {
-  // const image = fs.readFileSync("YOUR_IMAGE.jpg", {
-  //   encoding: "base64"
-  // })
-  axios({
-    method: "POST",
-    url: "https://detect.roboflow.com/fridgesmart/1",
-    params: {
-        api_key: process.env.REACT_APP_ROBO_API_KEY,
-        image: "https://i.imgur.com/6TTIhYL.jpeg"
-    }
-  })
-  .then(function(response) {
-    let ingredients = []
-    response.data.predictions.map((prediction) => (ingredients.push(prediction.class)))
-    console.log(ingredients)
-  })
-  .catch(function(error) {
-      console.log(error.message);
-  });
-}
-
 const Upload = () => {
+  const [Recipes, setRecipes] = useState([]);
+
+  const processImage = () => {
+    // const image = fs.readFileSync("YOUR_IMAGE.jpg", {
+    //   encoding: "base64"
+    // })
+    axios({
+      method: "POST",
+      url: "https://detect.roboflow.com/fridgesmart/1",
+      params: {
+          api_key: process.env.REACT_APP_ROBO_API_KEY,
+          image: "https://i.imgur.com/6TTIhYL.jpeg"
+      }
+    })
+    .then(function(response) {
+      let ingredients = []
+      response.data.predictions.map((prediction) => (ingredients.push(prediction.class)))
+      setRecipes(Recipes.concat(<Recipes ingredients={ingredients}/>))
+      console.log(ingredients)
+    })
+    .catch(function(error) {
+        console.log(error.message);
+    });
+  }
+
   return (
     <div>
       <button onClick={processImage}>upload</button>
+      {Recipes}
     </div>
   )
 }
